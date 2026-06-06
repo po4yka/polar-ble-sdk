ccdcba609f0e1837c087c20851c446ea9a6d03fe
Nikita Pochaev <pochaev.nik@gmail.com>
2026-06-06 12:29:59 +0400

    Route iOS advertisement name utilities through shared planner

--- sources/iOS/ios-communications/Sources/iOSCommunications/ble/api/model/polar/PolarAdvDataUtility.swift
+++ sources/iOS/ios-communications/Sources/iOSCommunications/ble/api/model/polar/PolarAdvDataUtility.swift
@@ -6,13 +6,24 @@ import PolarBleSdkShared
 open class PolarAdvDataUtility {
         
     public static func getDeviceNameFromAdvLocalName(advLocalName: String, withPrefixToTrim prefix: String = "Polar") -> String {
-        #if canImport(PolarBleSdkShared)
-        return PolarIosSharedBridge.shared.advertisementDeviceModelName(localName: advLocalName, prefixToTrim: prefix)
-        #else
-        if (isValidDevice(advLocalName: advLocalName, requiredPrefix: prefix)) {
+        if let sharedModelName = PolarAdvertisementRuntimePlanner.deviceModelName(localName: advLocalName, prefixToTrim: prefix) {
+            return sharedModelName
+        }
+        return fallbackDeviceNameFromAdvLocalName(advLocalName: advLocalName, prefix: prefix)
+    }
+
+    public static func isValidDevice(advLocalName: String, requiredPrefix: String = "Polar") -> Bool {
+        if let sharedIsValid = PolarAdvertisementRuntimePlanner.isValidDeviceName(localName: advLocalName, requiredPrefix: requiredPrefix) {
+            return sharedIsValid
+        }
+        return fallbackIsValidDevice(advLocalName: advLocalName, requiredPrefix: requiredPrefix)
+    }
+
+    static func fallbackDeviceNameFromAdvLocalName(advLocalName: String, prefix: String) -> String {
+        if fallbackIsValidDevice(advLocalName: advLocalName, requiredPrefix: prefix) {
             let modelName = advLocalName.trimmingCharacters(in: .whitespacesAndNewlines)
                 .replacingOccurrences(of: prefix != "" ? prefix + " " : "", with: "")
-            
+
             if let endIndex = modelName.lastIndex(of: " ") {
                 let mySubstring = modelName[..<(endIndex)]
                 return String(mySubstring)
@@ -22,16 +33,11 @@ open class PolarAdvDataUtility {
         } else {
             return ""
         }
-        #endif
     }
-    
-    public static func isValidDevice(advLocalName: String, requiredPrefix: String = "Polar") -> Bool {
-        #if canImport(PolarBleSdkShared)
-        return PolarIosSharedBridge.shared.isValidAdvertisementDeviceName(localName: advLocalName, requiredPrefix: requiredPrefix)
-        #else
+
+    static func fallbackIsValidDevice(advLocalName: String, requiredPrefix: String = "Polar") -> Bool {
         return advLocalName.trimmingCharacters(in: .whitespacesAndNewlines).hasPrefix(requiredPrefix) &&
             advLocalName.trimmingCharacters(in: .whitespacesAndNewlines).split(separator: " ").count > 2
-        #endif
     }
 
     public static func extractDeviceModelFromName(_ deviceName: String) -> String {
@@ -42,5 +48,23 @@ open class PolarAdvDataUtility {
             .dropLast()
             .joined(separator: " ")
         return deviceModel
+    }
+}
+
+enum PolarAdvertisementRuntimePlanner {
+    static func deviceModelName(localName: String, prefixToTrim: String) -> String? {
+        #if canImport(PolarBleSdkShared)
+        return PolarIosSharedBridge.shared.advertisementDeviceModelName(localName: localName, prefixToTrim: prefixToTrim)
+        #else
+        return nil
+        #endif
+    }
+
+    static func isValidDeviceName(localName: String, requiredPrefix: String) -> Bool? {
+        #if canImport(PolarBleSdkShared)
+        return PolarIosSharedBridge.shared.isValidAdvertisementDeviceName(localName: localName, requiredPrefix: requiredPrefix)
+        #else
+        return nil
+        #endif
     }
 }
