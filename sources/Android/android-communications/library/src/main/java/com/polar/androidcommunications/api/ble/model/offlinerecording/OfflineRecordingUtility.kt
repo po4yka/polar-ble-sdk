76ddc28443bbae8a6ae6a658696fe5fa3815f4d9
Nikita Pochaev <pochaev.nik@gmail.com>
2026-06-12 14:26:52 +0400

    Route offline recording filename mapping through runtime adapter

--- sources/Android/android-communications/library/src/main/java/com/polar/androidcommunications/api/ble/model/offlinerecording/OfflineRecordingUtility.kt
+++ sources/Android/android-communications/library/src/main/java/com/polar/androidcommunications/api/ble/model/offlinerecording/OfflineRecordingUtility.kt
@@ -1,21 +1,11 @@
 package com.polar.androidcommunications.api.ble.model.offlinerecording
 
 import com.polar.androidcommunications.api.ble.model.gatt.client.pmd.PmdMeasurementType
-import com.polar.shared.sdk.PolarOfflineRecordingMeasurementType
-import com.polar.shared.sdk.PolarOfflineRecordingModels
+import com.polar.sdk.impl.utils.PolarRuntimePlannerAdapter
 
 internal object OfflineRecordingUtility {
 
     fun mapOfflineRecordingFileNameToMeasurementType(fileName: String): PmdMeasurementType {
-        return when (PolarOfflineRecordingModels.measurementTypeFromFileName(fileName)) {
-            PolarOfflineRecordingMeasurementType.ACC -> PmdMeasurementType.ACC
-            PolarOfflineRecordingMeasurementType.GYRO -> PmdMeasurementType.GYRO
-            PolarOfflineRecordingMeasurementType.MAGNETOMETER -> PmdMeasurementType.MAGNETOMETER
-            PolarOfflineRecordingMeasurementType.PPG -> PmdMeasurementType.PPG
-            PolarOfflineRecordingMeasurementType.PPI -> PmdMeasurementType.PPI
-            PolarOfflineRecordingMeasurementType.OFFLINE_HR -> PmdMeasurementType.OFFLINE_HR
-            PolarOfflineRecordingMeasurementType.TEMPERATURE -> PmdMeasurementType.TEMPERATURE
-            PolarOfflineRecordingMeasurementType.SKIN_TEMP -> PmdMeasurementType.SKIN_TEMP
-        }
+        return PmdMeasurementType.valueOf(PolarRuntimePlannerAdapter.offlineRecordingMeasurementTypeName(fileName))
     }
 }
